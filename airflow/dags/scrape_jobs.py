075c9e36ef6d5630bc410e2e78438b3139042f1b
nouhaylayousfi <nouhayla.yousfi23@ump.ac.ma>
2026-09-03 18:05:33 +0100

    add a silver job

--- airflow/dags/scrape_jobs.py
+++ airflow/dags/scrape_jobs.py
@@ -3,6 +3,7 @@ import os
 import logging 
 from datetime import datetime
 from airflow.sdk import dag , task 
+from airflow.providers.standard.operators.trigger_dagrun import TriggerDagRunOperator
 
 logger = logging.getLogger(__name__)
 
@@ -75,7 +76,7 @@ def scrape_jobs():
         from services.ingestion.scrapers.indeed import IndeedApifyConnector
         from services.ingestion.producers.bronze_writer import write_to_bronze
 
-        connector = IndeedApifyConnector(country="MA", max_items=100)
+        connector = IndeedApifyConnector(country="MA", max_items=10)
         offers = connector.scrape()
 
         logger.info("Indeed MA (Apify) : %d offers collected", len(offers))
@@ -89,7 +90,7 @@ def scrape_jobs():
         from services.ingestion.scrapers.indeed import IndeedApifyConnector
         from services.ingestion.producers.bronze_writer import write_to_bronze
     
-        connector = IndeedApifyConnector(country="FR", max_items=100)
+        connector = IndeedApifyConnector(country="FR", max_items=10)
         offers    = connector.scrape()
     
         logger.info("Indeed FR (Apify) : %d offers collected", len(offers))
@@ -97,11 +98,68 @@ def scrape_jobs():
         logger.info("Indeed FR (Apify) : written to %s", key)
         return key
 
-    ingest_france_travail()
-    ingest_adzuna()
-    ingest_rekrute()
-    ingest_indeed_ma()
-    ingest_indeed_fr()
+    @task(task_id="collcet_bronze_keys")
+    def collect_bronze_keys(keys: list[str]) -> list[str]:
+        return [k for k in keys if k]
 
+    ft_key = ingest_france_travail()
+    adzuna_key = ingest_adzuna()
+    rekrute_key = ingest_rekrute()
+    indeed_ma_key = ingest_indeed_ma()
+    indeed_fr_key = ingest_indeed_fr()
+
+    rekrute_key >> indeed_ma_key >> indeed_fr_key
+
+    all_keys = collect_bronze_keys([ft_key, adzuna_key, rekrute_key, indeed_ma_key, indeed_fr_key])
+
+    trigger_silver = TriggerDagRunOperator(
+        task_id="trigger_silver_layer",
+        trigger_dag_id="silver_layer",
+        conf={"bronze_keys": all_keys} # will be transmitted to the Silver DAG
+    )
+
+    all_keys >> trigger_silver
+
+@dag(
+    dag_id="silver_layer",
+    schedule=None,
+    start_date=datetime(2024,1,1),
+    catchup=False,
+    tags=["silver", "transformations", "jobs"],
+    doc_md="""
+    
+    """
+)
+def silver_layer():
+
+    @task(task_id="transform_silver")
+    def transform_silver(**context) -> str:
+        from services.silver.silver_job import run_silver_job, create_spark_session_minio
+        from services.silver.silver_writer import write_to_silver
+        import os
+
+        bronze_keys = context["dag_run"].conf.get("bronze_keys", [])
+        if not bronze_keys:
+            logger.warning("No new Bronze files to process - skipping Silver run")
+            return ""
+
+        bucket = os.getenv("MINIO_BUCKET")
+        paths = [f"s3a://{bucket}/{key}" for key in bronze_keys]
+        spark = create_spark_session_minio(
+            os.getenv('SPARK_MASTER_URL'),
+            os.getenv('MINIO_ENDPOINT'),
+            os.getenv('MINIO_ACCESS_KEY'),
+            os.getenv('MINIO_SECRET_KEY')
+        )
+        df_bronze = spark.read.option("multiline", "true").json(paths)
+        df_silver = run_silver_job(df_bronze)
+
+        result_path = write_to_silver(df_silver, source="offres", mode="append")
+        logger.info("Silver :written to %s", result_path)
+        return result_path
+
+    transform_silver()
+    
 
 scrape_jobs()
+silver_layer()
